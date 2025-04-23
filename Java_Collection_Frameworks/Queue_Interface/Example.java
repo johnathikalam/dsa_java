ba49099ae190c1b7a95014417d7cfa0d3b7ccf96
John Joseph <john.athikalam@calpinetech.com>
2025-04-23 16:58:38 +0530

    added result as comment

--- Java_Collection_Frameworks/Queue_Interface/Example.java
+++ Java_Collection_Frameworks/Queue_Interface/Example.java
@@ -7,19 +7,19 @@ public class Example{
         q.add(10);  //add element to list
         q.add(20);
         q.add(30);
-        System.out.println("Queue :"+q);
+        System.out.println("Queue :"+q);    //[10, 20, 30]
         
-        System.out.println("Element :"+q.element());    //retrive head but doesn't remove head
+        System.out.println("Element :"+q.element());    //10, retrive head but doesn't remove head
 
-        System.out.println("Peek :"+q.peek());   //retrive head, but doesn't remove head and returns null if queue is empty
-        System.out.println("Queue :"+q);
+        System.out.println("Peek :"+q.peek());   //10, retrive head, but doesn't remove head and returns null if queue is empty
+        System.out.println("Queue :"+q);    //[10, 20, 30]
 
-        System.out.println("Poll :"+q.poll());   //retrives and remove head, also returns null if queue is empty 
-        System.out.println("Queue :"+q);
+        System.out.println("Poll :"+q.poll());   //10, retrives and remove head, also returns null if queue is empty 
+        System.out.println("Queue :"+q);    //[20, 30]
 
-        System.out.println("Remove :"+q.remove()); //retrive and remove head
-        System.out.println("Queue :"+q);
+        System.out.println("Remove :"+q.remove()); //20, retrive and remove head
+        System.out.println("Queue :"+q);    //[30]
 
-        System.out.println("Size :"+q.size());   //returns the size of queue
+        System.out.println("Size :"+q.size());   //1, returns the size of queue
     }
 }
