b7b74bdc427ce2a4612665666d9028286a29d19d
John Joseph <john.athikalam@calpinetech.com>
2025-04-23 16:57:33 +0530

    added result as comment

--- Java_Collection_Frameworks/Map_Interface/Example.java
+++ Java_Collection_Frameworks/Map_Interface/Example.java
@@ -13,36 +13,34 @@ public class Example {
         m1.put("Mahnaz", 31);
         m1.put("Ayan", 12);
         m1.put("Daisy", 14);
-        System.out.println("Map m1: " + m1);
+        System.out.println("Map m1: " + m1);    //{Joseph=29, John=22, Daisy=14, Ayan=12, Zara=8, Mahnaz=31}
 
         m2.putAll(m1);  //to add multiple key value pairs
-        System.out.println("Map m2: " + m2);
+        System.out.println("Map m2: " + m2);    //{Ayan=12, Daisy=14, John=22, Joseph=29, Mahnaz=31, Zara=8}
 
-        System.out.println("ContainsValue : " + m1.containsValue(22));  //return true if value is present else false
-        System.out.println("ContainsValue : " + m1.containsValue(2));
-
-
-        System.out.println("ContainsKey : " + m1.containsKey("John"));  //return true if key is present else false
-        System.out.println("ContainsKey : " + m1.containsKey("Jo"));
-
-        System.out.println("Entryset : " + m1.entrySet());  //return entrySet of map
-
-        System.out.println("Equals : " + m1.equals(m2));    //check whether the key value pairs in m2 is in the m1
-
-        System.out.println("Get  : "+ m1.get("John"));  //return value of given key
+        System.out.println("ContainsValue : " + m1.containsValue(22));  //true, return true if value is present else false
+        System.out.println("ContainsValue : " + m1.containsValue(2));   //fasle
 
 
+        System.out.println("ContainsKey : " + m1.containsKey("John"));  //true, return true if key is present else false
+        System.out.println("ContainsKey : " + m1.containsKey("Jo"));    //false
 
-        System.out.println(m1.hashCode());  //return hashcode for map
+        System.out.println("Entryset : " + m1.entrySet());  //[Joseph=29, John=22, Daisy=14, Ayan=12, Zara=8, Mahnaz=31], return entrySet of map
 
-        System.out.println(m1.isEmpty());   //check whether the map is empty or not
+        System.out.println("Equals : " + m1.equals(m2));    //true, check whether the key value pairs in m2 is in the m1
 
-        System.out.println(m1.keySet());    //return keyset
+        System.out.println("Get  : "+ m1.get("John"));  //22, return value of given key
 
-        System.out.println("Size : " + m1.size());  //return size of map
+        System.out.println(m1.hashCode());  //299802750, return hashcode for map
 
-        System.out.println("Remove : " + m1.remove("John"));    //remove the key value pair
+        System.out.println(m1.isEmpty());   //false, check whether the map is empty or not
 
-        System.out.println("Values : " + m1.values());  //return the list of values
+        System.out.println(m1.keySet());    //[Joseph, John, Daisy, Ayan, Zara, Mahnaz], return keyset
+
+        System.out.println("Size : " + m1.size());  //6, return size of map
+
+        System.out.println("Remove : " + m1.remove("John"));    //22, remove the key value pair
+
+        System.out.println("Values : " + m1.values());  //[29, 14, 12, 8, 31], return the list of values
     }  
 }
