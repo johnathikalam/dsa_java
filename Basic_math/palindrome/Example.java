150b10ba062108a3e62cac6d4b74e16da49aad7a
John Joseph <john.athikalam@calpinetech.com>
2025-05-07 16:22:52 +0530

    Added number palindrome check

--- Basic_math/palindrome/Example.java
+++ Basic_math/palindrome/Example.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Example {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number : ");
-        int N = sc.nextInt();
+    public static boolean palindrome(int N){
         int num = N;
         int rev = 0;
         while (num > 0){
@@ -12,10 +9,25 @@ public class Example {
             num /= 10;
         }
         if (rev == N){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }    
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number : ");
+        int N = sc.nextInt();
+
+        if (palindrome(N)){
             System.out.println("Number is palindrome");
         }
         else{
             System.out.println("Number is not a palindrome");
         }
-    }    
+    }
+    
 }
