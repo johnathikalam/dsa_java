970d68a6ceda675f510aebf192db3b58f64871f1
johnathikalam <johnathikalam3@gmail.com>
2025-06-21 17:58:43 +0530

    added different approches using hash map ans two pointer for finding largest sum sub array

--- Array_questions/Easy/sum_sub_array/Example.java
+++ Array_questions/Easy/sum_sub_array/Example.java
@@ -1,19 +1,64 @@
+import java.util.*;
 public class Example{
-    public static int long_sum_brute(int [] arr, int k){
-        int long_length = 0;
+    public static int longSumBrute(int [] arr, int k){
+        int maxLength = 0;
         for (int i = 0; i < arr.length; i++){
             int s = 0;
             for (int j = i; j < arr.length; j++){
                 s += arr[j];
-                if (s == k && long_length < j-i+1) long_length = j-i+1;  
+                if (s == k && maxLength < j-i+1) maxLength = j-i+1;  
             }
         }
-        return long_length;
+        return maxLength;
+    }
+
+    public static int longSumHash(int [] arr, int k){
+        Map <Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLength = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+
+            if (sum == k && maxLength < i+1) maxLength = i+1;
+
+            long rem = sum - k;
+
+            if (preSumMap.containsKey(rem)){
+                int len = i - preSumMap.get(rem);
+                if (maxLength < len) maxLength = len;
+            }
+
+            if (!preSumMap.containsKey(sum)){
+                preSumMap.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+
+    public static int longSumTwoPointer(int [] arr, int k){
+        long sum = arr[0];
+        int i = 0, j = 0;
+        int maxLength = 0;
+        int n = arr.length;
+
+        while (j < n){
+            while (i <= j && sum > k){
+                sum -= arr[i];
+                i += 1;
+            }
+            
+            if (sum == k && maxLength < j - i + 1) maxLength = j - i + 1;
+
+            j += 1;
+            if (j < n ) sum += arr[j];
+        }
+        return maxLength;
     }
 
     public static void main(String [] args){
-        int [] arr = {2,3,5,1,9};
-        int res = long_sum_brute(arr,10);
+        int [] arr = {2,0,0,0,0,3};
+        // int [] arr = {2,3,5,1,9};
+        int res = longSumTwoPointer(arr, 3);
         System.out.println(res);
     }
 
